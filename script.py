6fe8dacb99172fa05c83ee8d75b90328a2e0867b
FOURWORDSALLCAPS <48273739+FOURWORDSALLCAPS@users.noreply.github.com>
2023-04-29 15:43:09 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -3,22 +3,14 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 import os
 
-load_dotenv("dev.env")
-
-TOKEN = os.getenv('TOKEN')
-
-token = {
-	'Authorization': f'Bearer {TOKEN}'
-}
-
 user_input = (
-	input('Введите ссылку: ')
+	input('Введите ссылку: ')	
 )
 
 json = {
 	'long_url': f'{user_input}'
 }
-
+ 
 url = (
 	'https://api-ssl.bitly.com/v4/shorten'
 )
@@ -26,39 +18,56 @@ url = (
 link = (
 	f'https://api-ssl.bitly.com/v4/bitlinks/{user_input}/clicks/summary'
 )
+	
+url_bitlink = (
+	f'https://api-ssl.bitly.com/v4/bitlinks/{user_input}'
+)
 
 params = {
 	'unit ': 'minute',
 	'units ': '-1'
 }
-def is_bitlink(user_input):
-	url_test = f'{user_input}'
-	parsed = urlparse(url_test)
-	if parsed.path[0:6] == 'bit.ly':
-		def count_clicks(token, link):
-			response = requests.get(link, headers=token, params=params)
-			response.raise_for_status()
-			return response.text[60]	
 
-		try:
-			if __name__ == '__main__':
-  				print ('По вашей ссылке прошли:', count_clicks(token, link), 'раз(а)')
-		except requests.exceptions.HTTPError:
-			print('Неправильная ссылка')
-
-	else:
-		def shorten_link(token, url, json):
-			response = requests.post(url, headers=token, json=json)
-			response.raise_for_status()
-			return response.text[47:61]
-
-		try:
-			if __name__ == '__main__':
-				print ('Битлинк:', shorten_link(token, url, json))
-		except requests.exceptions.HTTPError:
-			print('Неправильная ссылка')
+def is_bitlink(token, url_bitlink):
+	response = requests.get(url_bitlink, headers=token)
+	test = response.ok
+	return test
 	
-is_bitlink(user_input)
+def count_clicks(token, link):
+	response = requests.get(link, headers=token, params=params)
+	response.raise_for_status()
+	test = response.json()
+	return test['total_clicks']
+
+def shorten_link(token, url, json):
+	response = requests.post(url, headers=token, json=json)
+	response.raise_for_status()
+	test = response.json()
+	return test['id']
+	
+def main():
+	if is_bitlink(token, url_bitlink) == True:
+		try:
+  			print ('По вашей ссылке прошли:', count_clicks(token, link), 'раз(а)')
+		except requests.exceptions.HTTPError:
+			print('Неправильная ссылка')
+	else:
+		try:
+			print ('Битлинк:', shorten_link(token, url, json))
+		except requests.exceptions.HTTPError:
+			print('Неправильная ссылка')
+	 
+if __name__ == '__main__':
+	load_dotenv("dev.env")
+	TOKEN = os.getenv('Access_Token')	
+	token = {
+		'Authorization': f'Bearer {TOKEN}'
+	}
+	main()	
+
+
+
+
 
 
 
