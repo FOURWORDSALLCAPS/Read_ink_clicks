ce85c46184810b1a60721f2eab42694cd1b4619b
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-05-03 13:46:56 +0300

    Replaced 'inpunt' with 'argparse'

--- script.py
+++ script.py
@@ -5,8 +5,9 @@ import os
 import argparse
 
 
-def is_bitlink(authorization, args):
-    args_link = args.l
+def is_bitlink(token, user_url):
+    authorization = {'Authorization': f'Bearer {token}'}
+    args_link = user_url.l
     http_test = urlparse(f'{args_link}')
     parse_1 = http_test.hostname
     parse_2 = http_test.path
@@ -16,8 +17,9 @@ def is_bitlink(authorization, args):
     return test
 
 
-def count_clicks(authorization, args):
-    http_test = urlparse(f'{args}')
+def count_clicks(token, user_url):
+    authorization = {'Authorization': f'Bearer {token}'}
+    http_test = urlparse(f'{user_url}')
     parse_1 = http_test.hostname
     parse_2 = http_test.path
     link = f'https://api-ssl.bitly.com/v4/bitlinks/{parse_1}{parse_2}/clicks/summary'
@@ -31,9 +33,10 @@ def count_clicks(authorization, args):
     return test['total_clicks']
 
 
-def shorten_link(authorization, args):
+def shorten_link(token, user_url):
+    authorization = {'Authorization': f'Bearer {token}'}
     url = 'https://api-ssl.bitly.com/v4/shorten'
-    json = {'long_url': f'{args}'}
+    json = {'long_url': f'{user_url}'}
     response = requests.post(url, headers=authorization, json=json)
     response.raise_for_status()
     test = response.json()
@@ -45,14 +48,13 @@ def main():
         description='Программа может сжимать ссылки или выдавать количество переходов по ним'
     )
     parser.add_argument('l', help='Введите ссылку')
-    args = parser.parse_args()
-    token_authorization = os.getenv('ACCESS_BITLY_TOKEN')
-    authorization = {'Authorization': f'Bearer {token_authorization}'}
+    user_url = parser.parse_args()
+    token = os.getenv('ACCESS_BITLY_TOKEN')
     try:
-        if not is_bitlink(authorization, args):
-            print('Битлинк:', shorten_link(authorization, args.l))
+        if not is_bitlink(token, user_url):
+            print('Битлинк:', shorten_link(token, user_url.l))
         else:
-            print('По вашей ссылке прошли:', count_clicks(authorization, args.l), 'раз(а)')
+            print('По вашей ссылке прошли:', count_clicks(token, user_url.l), 'раз(а)')
     except requests.exceptions.HTTPError:
         print('Неправильная ссылка')
 
